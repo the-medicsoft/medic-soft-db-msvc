dd1e0bdd8d57aaf860222428d72fbd2b89837a9c
Aakash Verma <aakashvermaee@gmail.com>
2019-05-29 19:41:21 +0530

    initial work on doctor CRUD & schema functions

--- doctor/doctor.js
+++ doctor/doctor.js
@@ -4,24 +4,164 @@ const { userSchema } = require("../db/shared_schemas/");
 const doctorSchema = new mongoose.Schema(userSchema);
 
 doctorSchema.add({
-  isAdmin: {
-    type: Boolean,
-    default: false
-  },
-  DOJ: Date,
-  qualifications: [
-    {
-      type: String
-    }
-  ],
-  specialisations: [
-    {
-      type: String
-    }
-  ],
-  designation: { type: String, required: true },
-  department: { type: String, required: true },
-  visitingTime: [{ branch: String, timings: Date }]
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    DOJ: Date,
+    qualifications: [
+        {
+            type: String
+        }
+    ],
+    specialisations: [
+        {
+            type: String
+        }
+    ],
+    designation: { type: String, required: true },
+    department: { type: String },
+    visitingTime: [{ branch: String, timings: Date }]
 });
+
+doctorSchema.statics.getIsDoctorAdminByEmail = async function (email) {
+    try {
+        let doctor = await this.findOne({ "contacts.email": email }, 'isAdmin');
+
+        if (doctor.isAdmin) {
+            console.log(doctor);
+        }
+    } catch (err) { }
+};
+
+doctorSchema.statics.getDoctors = async function () {
+    try {
+        // remove _id, _v and password properties from resultset
+        const doctorResultFilter = '-_id -__v -password';
+
+        let doctors = await this.find({}, doctorResultFilter);
+
+        if (doctors.length) {
+            return {
+                success: true,
+                statusCode: 200,
+                statusText: "OK",
+                total: doctors.length,
+                data: doctors
+            };
+        }
+        else throw new Error();
+    } catch (err) {
+        return {
+            success: false,
+            statusCode: 404,
+            statusText: "Not Found",
+            message: "Not Found"
+        };
+    }
+}
+
+doctorSchema.statics.getDoctorByEmail = async function (email) {
+    try {
+        // remove _id and _v properties from resultset
+        const doctorResultFilter = '-_id -__v';
+
+        let doctor = await this.findOne({ "contacts.email": email }, doctorResultFilter);
+
+        if (doctor)
+            return {
+                success: true,
+                statusCode: 200,
+                statusText: "OK",
+                message: 'doctor found!',
+                data: { doctor }
+            };
+        else if(!doctor)
+            return {
+                success: false,
+                statusCode: 404,
+                statusText: "Not Found",
+                message: 'doctor Not found!'
+            };
+        else throw new Error();
+    } catch (err) {
+        return {
+            success: false,
+            statusCode: 500,
+            statusText: "Internal Server Error",
+            message: err.message
+        };
+    }
+};
+
+doctorSchema.statics.getDoctorByQuery = async function (query) {
+    try {
+        // remove _id and _v properties from resultset
+        const doctorResultFilter = '-_id -__v';
+
+        let doctors = await this.find(query, doctorResultFilter);
+
+        if (doctors.length) {
+            return {
+                success: true,
+                statusCode: 200,
+                statusText: "OK",
+                total: doctors.length,
+                data: doctors
+            };
+        }
+        else throw new Error();
+    } catch (err) {
+        return {
+            success: false,
+            statusCode: 404,
+            statusText: "Not Found",
+            message: "Not Found"
+        };
+    }
+}
+
+doctorSchema.statics.createDoctor = async function (newDoctor) {
+    try {
+        // if doctor exists then do not insert and, prompt user with message
+        let { email } = newDoctor.contacts;
+
+        let doctorExists = await this.findOne({ "contacts.email": email });
+
+        if (doctorExists && (doctorExists.contacts.email === email)) {
+            return {
+                success: false,
+                statusCode: 409,
+                statusText: "Conflict",
+                message: `doctor with email ${email} already exists!`
+            };
+        }
+
+        let doctor = await this.create(newDoctor)
+
+        if (doctor && doctor.contacts.email === email) {
+            return {
+                success: true,
+                statusCode: 200,
+                statusText: "OK",
+                message: `doctor inserted`,
+                data: doctor
+            };
+        } else {
+            throw new Error();
+        }
+    } catch (err) {
+        return {
+            success: false,
+            statusCode: 500,
+            statusText: "Internal Server Error",
+            message: err.message
+        };
+    }
+};
+
+doctorSchema.statics.updateDoctorByEmail = async function (email, updateDoctorWith) { };
+
+doctorSchema.statics.deleteDoctorByEmail = async function (email) { };
 
 exports.Doctor = mongoose.model("Doctor", doctorSchema);
--- doctor/doctors.js
+++ doctor/doctors.js
@@ -2,16 +2,51 @@ const { Doctor } = require("./doctor");
 
 exports.getDoctors = async (req, res) => {
   try {
+    if (req.query) {
+      return getDoctorByQuery(req, res);
+    }
+
+    let response = await Doctor.getDoctors();
+    
+    res
+      .code(response.statusCode)
+      .send(response);
+  } catch (err) {}
+};
+
+const getDoctorByQuery = async (req, res) => {
+  try {
+    let response = await Doctor.getDoctorByQuery(req.query);
+
+    res
+      .code(response.statusCode)
+      .send(response);
   } catch (err) {}
 };
 
 exports.getDoctorByEmail = async (req, res) => {
   try {
-  } catch (err) {}
+    let response = await Doctor.getDoctorByEmail(req.params.email);
+
+    res
+      .code(response.statusCode)
+      .send(response);
+  } catch (err) {
+    res
+      .code(err.statusCode)
+      .send(err);
+  }
 };
 
 exports.createDoctor = async (req, res) => {
   try {
+    let doctor = new Doctor(req.body);
+
+    let response = await Doctor.createDoctor(doctor);
+    
+    res
+      .code(response.statusCode)
+      .send(response);
   } catch (err) {}
 };
 
